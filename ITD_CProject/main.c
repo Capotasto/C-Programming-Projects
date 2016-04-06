a2363175441d1cd0c4a944eafca43e2c6bb99878
Capotasto <rocker519804@gmail.com>
2016-04-06 10:31:52 -0700

    Initial Commit

---
+++ ITD_CProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ITD_CProject
+//
+//  Created by Norio Egi on 4/6/16.
+//  Copyright © 2016 Capotasto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
