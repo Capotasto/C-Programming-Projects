f0c156d138e3312e8144e748688c07fd3e015307
Capotasto <rocker519804@gmail.com>
2016-04-06 12:01:47 -0700

    add counterControl

---
+++ ITD_CProject/counterControl.c
@@ -0,0 +1,9 @@
+//
+//  counterControl.c
+//  ITD_CProject
+//
+//  Created by Norio Egi on 4/6/16.
+//  Copyright © 2016 Capotasto. All rights reserved.
+//
+
+#include "counterControl.h"
---
+++ ITD_CProject/counterControl.h
@@ -0,0 +1,14 @@
+//
+//  counterControl.h
+//  ITD_CProject
+//
+//  Created by Norio Egi on 4/6/16.
+//  Copyright © 2016 Capotasto. All rights reserved.
+//
+
+#ifndef counterControl_h
+#define counterControl_h
+
+#include <stdio.h>
+
+#endif /* counterControl_h */
